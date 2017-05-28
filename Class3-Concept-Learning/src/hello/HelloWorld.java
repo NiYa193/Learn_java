c929f3b79cc56957e8043784dde308f5761f2de4
NiYa193 <1556579665@qq.com>
2017-05-28 16:45:37 +0800

    基本数据类型学习

--- Class3-Concept-Learning/src/hello/HelloWorld.java
+++ Class3-Concept-Learning/src/hello/HelloWorld.java
@@ -2,9 +2,14 @@ package hello;
 
 public class HelloWorld{
 	public static void main(String[] args){
-		String cnWord = "ÎÒ°®Ä½¿Î";
-		String enWord = "I love imooc";
-		System.out.println(cnWord);
-		System.out.println(enWord);
+		String name = "°®Ä½¿Î";
+		char sex = 'ÄÐ';
+		int awardNum = 18;
+		double price = 120.5;
+		boolean shutFlag = true;
+		System.out.println(name);
+		System.out.println(sex);
+		System.out.println(awardNum);
+		System.out.println(shutFlag);
 	}
 }
