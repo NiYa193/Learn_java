ee6e39de776245200f6eabe6f4bbf5fae8d1c548
NiYa193 <1556579665@qq.com>
2017-05-28 17:30:39 +0800

    Java的常量声明学习

--- Class3-Concept-Learning/src/hello/HelloWorld.java
+++ Class3-Concept-Learning/src/hello/HelloWorld.java
@@ -2,10 +2,9 @@ package hello;
 
 public class HelloWorld{
 	public static void main(String[] args){
-		double avg1 = 78.5;
-		int rise = 5;
-		double avg2 = avg1 + rise;
-		System.out.println("考试平均分："+avg1);
-		System.out.println("调整后的平均分："+avg2);
+		final String SEXMEN = "男";
+		final String SEXWOMEN = "女";
+		System.out.println(SEXMEN);
+		System.out.println(SEXWOMEN);
 	}
 }
