1cd87fc243ecbe2a88eb1c1f579856adee74f39a
NiYa193 <1556579665@qq.com>
2017-05-28 20:32:22 +0800

    逻辑运算符学习

--- Class3-Concept-Learning/src/hello/HelloWorld.java
+++ Class3-Concept-Learning/src/hello/HelloWorld.java
@@ -1,14 +1,14 @@
-package hello;
+package hello; 
 
-public class HelloWorld{
+public class HelloWorld {
     public static void main(String[] args) {
-		int a=16;
-		double b=9.5;
-		String str1="hello";
-		String str2="imooc";
-		System.out.println("a等于b：" + (a == b));
-		System.out.println("a大于b：" + (a > b));
-		System.out.println("a小于等于b：" + (a <= b));
-		System.out.println("str1等于str2：" + (str1 == str2));
+		boolean a = true; // a同意
+		boolean b = false; // b反对
+		boolean c = false; // c反对
+		boolean d = true; // d同意
+	    System.out.println((a && b) + "未通过");
+        System.out.println((a || b) + "通过");
+        System.out.println((!a) + "未通过");
+        System.out.println((c ^ d) + "通过");
 	}
 }
