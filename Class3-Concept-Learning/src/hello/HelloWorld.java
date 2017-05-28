b58f62f11b9e243ff8650ab03c01f9c064260f16
NiYa193 <1556579665@qq.com>
2017-05-28 17:11:28 +0800

    自动类型转换学习

--- Class3-Concept-Learning/src/hello/HelloWorld.java
+++ Class3-Concept-Learning/src/hello/HelloWorld.java
@@ -2,7 +2,10 @@ package hello;
 
 public class HelloWorld{
 	public static void main(String[] args){
-		String city = "山东";
-		System.out.println("我的家乡在"+city);
+		double avg1 = 78.5;
+		int rise = 5;
+		double avg2 = avg1 + rise;
+		System.out.println("考试平均分："+avg1);
+		System.out.println("调整后的平均分："+avg2);
 	}
 }
