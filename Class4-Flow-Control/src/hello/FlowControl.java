b474788188a3da9c3972198c62eba91d778eb29a
NiYa193 <1556579665@qq.com>
2017-05-29 21:15:19 +0800

    if语句学习

--- Class4-Flow-Control/src/hello/FlowControl.java
+++ Class4-Flow-Control/src/hello/FlowControl.java
@@ -1,5 +1,10 @@
 package hello;
 
-public class FlowControl {
-
+public class FlowControl{
+	public static void main(String[] args){
+		int score = 95;
+		if(score > 90){
+			System.out.println("≥…º®”≈–„");
+		}
+	}
 }
