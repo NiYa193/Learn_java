17f847d5d7017e31eec4fb3d6a7b93f12e8c3778
NiYa193 <1556579665@qq.com>
2017-05-29 22:02:54 +0800

    switch语句学习

--- Class4-Flow-Control/src/hello/FlowControl.java
+++ Class4-Flow-Control/src/hello/FlowControl.java
@@ -1,15 +1,21 @@
 package hello;
 
 public class FlowControl{
-	public static void main(String[] args){
-		int score = 94;
-		String sex = "女";
-		if(score > 80){
-			if(sex == "女"){
-				System.out.println("进入女子组决赛");
-			}else{
-				System.out.println("进入男子组决赛");
-			}
-		}
+    public static void main(String[] args) {
+		char today='日';
+        switch(today){
+            case '一':
+            case '三':
+            case '五':
+                System.out.println("吃包子");
+                break;
+            case '二':
+            case '四':
+            case '六':
+                System.out.println("吃油条");
+                break;
+            case '日':
+                System.out.println("吃主席套餐");
+        }
 	}
 }
