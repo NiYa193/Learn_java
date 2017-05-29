c19cc6ef3a8439debd4622191a0cc29b142e4ef3
NiYa193 <1556579665@qq.com>
2017-05-29 21:26:49 +0800

    else if语句学习

--- Class4-Flow-Control/src/hello/FlowControl.java
+++ Class4-Flow-Control/src/hello/FlowControl.java
@@ -3,10 +3,14 @@ package hello;
 public class FlowControl{
 	public static void main(String[] args){
 		int age = 25;
-		if(age > 18){
+		if(age > 60){
 			System.out.println("成年");
+		}else if(age > 40){
+			System.out.println("中年");
+		}else if(age > 18){
+			System.out.println("少年");
 		}else{
-			System.out.println("未成年");
+			System.out.println("童年");
 		}
 	}
 }
