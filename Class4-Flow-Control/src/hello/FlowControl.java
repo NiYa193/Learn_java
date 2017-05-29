75bb95cf210124f094fe1e23d05cf59ce785f6b2
NiYa193 <1556579665@qq.com>
2017-05-30 07:56:27 +0800

    break语句的学习

--- Class4-Flow-Control/src/hello/FlowControl.java
+++ Class4-Flow-Control/src/hello/FlowControl.java
@@ -3,11 +3,12 @@ package hello;
 public class FlowControl{
 	public static void main(String[] args){
 		int sum = 0;
-		for(int i = 0; i <= 100; i++){
-			if(i % 3 != 0){
-				sum += i; 
+		for(int i = 1; i <= 10; i++){
+			sum += i;
+			if(sum > 20){
+				System.out.println("当前的累加值为："+sum);
+				break;
 			}
 		}
-		System.out.println("1到100之间不能被3整除的数之和为："+sum);
 	}
 }
