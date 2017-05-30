534c8182fe2db053adbbdab2fd679de6b4e1f952
NiYa193 <1556579665@qq.com>
2017-05-30 09:00:03 +0800

    加分算法问题

--- Class4-Flow-Control/src/hello/FlowControl.java
+++ Class4-Flow-Control/src/hello/FlowControl.java
@@ -1,12 +1,15 @@
 package hello;
 
 public class FlowControl{
-	public static void main(String[] args){
-		int num = 999;
+	public static void main(String[]args){
 		int count = 0;
-		for(; num != 0; num /= 10){
+		int score = 53;
+		System.out.println("加分前的分数："+score);
+		while(score<60){
+			score++;
 			count++;
 		}
-		System.out.println("它是个"+count+"位的数");
+		System.out.println("加分前的分数："+score);
+		System.out.println("共加了"+count+"次！");
 	}
 }
