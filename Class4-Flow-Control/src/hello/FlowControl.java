e76caa555dbbe185c573549ecf096073347e1e1f
NiYa193 <1556579665@qq.com>
2017-05-30 10:43:23 +0800

    第六章数组学习-基础

--- Class4-Flow-Control/src/hello/FlowControl.java
+++ Class4-Flow-Control/src/hello/FlowControl.java
@@ -1,25 +1,12 @@
 package hello;
-import java.util.Scanner;
 
 public class FlowControl{
 	public static void main(String[] args) {
-		int classNum = 3;
-		int stuNum = 4;
-		double sum = 0;
-		double score = 0;
-		double stuAvg = 0;
-		Scanner input = new Scanner(System.in);
-		for(int i = 1; classNum >= i ; i++){
-			System.out.println("请输入第"+i+"个班级的学生信息：");
-			for(int j =1; stuNum >= j; j++){
-				System.out.print("请输入第"+j+"个学生的分数：");
-				score = input.nextInt();
-				sum += score;
-			}
-			stuAvg = sum / stuNum;
-			System.out.println("第"+i+"个班级学生的平均分是"+stuAvg);
-			sum = 0;
-		}
-		
+		String[] subjects = new String[4];
+		subjects[0] = "物理";
+		subjects[1] = "化学";
+		subjects[2] = "生物";
+		subjects[3] = "语言";
+		System.out.println("第三个科目是"+subjects[2]);
 	}
 }
