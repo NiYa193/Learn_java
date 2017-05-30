4be5baaee69d4b327ff4b6ea4e8de8093537e540
NiYa193 <1556579665@qq.com>
2017-05-30 08:01:16 +0800

    continue语句的学习

--- Class4-Flow-Control/src/hello/FlowControl.java
+++ Class4-Flow-Control/src/hello/FlowControl.java
@@ -1,14 +1,14 @@
 package hello;
 
 public class FlowControl{
-	public static void main(String[] args){
+	public static void main(String[]args){
 		int sum = 0;
-		for(int i = 1; i <= 10; i++){
-			sum += i;
-			if(sum > 20){
-				System.out.println("当前的累加值为："+sum);
-				break;
+		for(int i =1; i <= 10; i++){
+			if(i % 2 != 0){
+				continue;
 			}
+			sum += i;
 		}
+		System.out.println("1到10之间所有的偶数的和为："+sum);
 	}
 }
