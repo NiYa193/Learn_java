91ba4904999d6b3c52d0193ac39491239f0ddc98
NiYa193 <1556579665@qq.com>
2017-05-30 09:06:05 +0800

    优化程序，添加可以手动输入成绩功能

--- Class4-Flow-Control/src/hello/FlowControl.java
+++ Class4-Flow-Control/src/hello/FlowControl.java
@@ -1,9 +1,12 @@
 package hello;
+import java.util.Scanner;
 
 public class FlowControl{
 	public static void main(String[]args){
+		Scanner input = new Scanner(System.in);//创建Scanner对象
+		System.out.print("请输入分数：");
 		int count = 0;
-		int score = 53;
+		int score = input.nextInt(); //获取用户输入信息
 		System.out.println("加分前的分数："+score);
 		while(score<60){
 			score++;
