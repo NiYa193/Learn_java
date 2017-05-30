c39982e4cbced6da463913b1042b449b31eee9c5
NiYa193 <1556579665@qq.com>
2017-05-30 11:26:38 +0800

    Arrays类学习

--- Class4-Flow-Control/src/hello/FlowControl.java
+++ Class4-Flow-Control/src/hello/FlowControl.java
@@ -1,25 +1,16 @@
 package hello;
 
+import java.util.Arrays;
+
 public class FlowControl {
     public static void main(String[] args) {
 		// 定义一个整型数组，并赋初值
-		int[] nums = new int[] { 61, 23, 4, 74, 13, 148, 20 };
-		int max = nums[0]; // 假定最大值为数组中的第一个元素
-		int min = nums[0]; // 假定最小值为数组中的第一个元素
-		double sum = 0;// 累加值
-		double avg = 0;// 平均值
+		String[] nums = new String[] {"sports", "game", "movie"};
+		//对数组内元素进行排序，从小到大
+		Arrays.sort(nums);
 		for(int i = 0; i < nums.length; i++){
-			if(nums[i] > max){
-				max = nums[i];
-			}
-			if(nums[i] < min){
-				min = nums[i];
-			}
-			sum += nums[i];
+			System.out.println(nums[i]);
 		}
-		avg = sum / nums.length;
-		System.out.println("数组中的最大值：" + max);
-		System.out.println("数组中的最小值：" + min);
-		System.out.println("数组中的平均值：" + avg);
+		System.out.println("组合后的字符串为："+Arrays.toString(nums));
 	}
 }
