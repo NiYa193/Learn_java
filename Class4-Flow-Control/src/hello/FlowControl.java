482145176fb9b2498819569ee161883897471a2e
NiYa193 <1556579665@qq.com>
2017-05-30 09:45:39 +0800

    第五章算法优化-平均分输入问题

--- Class4-Flow-Control/src/hello/FlowControl.java
+++ Class4-Flow-Control/src/hello/FlowControl.java
@@ -2,17 +2,24 @@ package hello;
 import java.util.Scanner;
 
 public class FlowControl{
-	public static void main(String[]args){
-		Scanner input = new Scanner(System.in);//创建Scanner对象
-		System.out.print("请输入分数：");
-		int count = 0;
-		int score = input.nextInt(); //获取用户输入信息
-		System.out.println("加分前的分数："+score);
-		while(score<60){
-			score++;
-			count++;
+	public static void main(String[] args) {
+		int classNum = 3;
+		int stuNum = 4;
+		double sum = 0;
+		double score = 0;
+		double stuAvg = 0;
+		Scanner input = new Scanner(System.in);
+		for(int i = 1; classNum >= i ; i++){
+			System.out.println("请输入第"+i+"个班级的学生信息：");
+			for(int j =1; stuNum >= j; j++){
+				System.out.print("请输入第"+j+"个学生的分数：");
+				score = input.nextInt();
+				sum += score;
+			}
+			stuAvg = sum / stuNum;
+			System.out.println("第"+i+"个班级学生的平均分是"+stuAvg);
+			sum = 0;
 		}
-		System.out.println("加分前的分数："+score);
-		System.out.println("共加了"+count+"次！");
+		
 	}
 }
