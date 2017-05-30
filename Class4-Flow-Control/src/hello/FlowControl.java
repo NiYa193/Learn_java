a11e9d544ced2a42eca8aa99bce29d43b9edc8cd
NiYa193 <1556579665@qq.com>
2017-05-30 08:07:44 +0800

    多重循环的学习

--- Class4-Flow-Control/src/hello/FlowControl.java
+++ Class4-Flow-Control/src/hello/FlowControl.java
@@ -2,13 +2,12 @@ package hello;
 
 public class FlowControl{
 	public static void main(String[]args){
-		int sum = 0;
-		for(int i =1; i <= 10; i++){
-			if(i % 2 != 0){
-				continue;
+		System.out.println("双重循环实现打印三角形");
+		for(int i = 0; i < 3; i++){
+			for(int j = 0; j <= i; j++){
+				System.out.print("*");
 			}
-			sum += i;
+			System.out.println();
 		}
-		System.out.println("1到10之间所有的偶数的和为："+sum);
 	}
 }
