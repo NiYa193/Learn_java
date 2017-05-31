12442de6dfa503540f726dea132f16cbe6a6cfb3
NiYa193 <1556579665@qq.com>
2017-05-31 22:02:56 +0800

    排序并输出前三名成绩题目

--- Class6-Print-score/src/hello/sortScore.java
+++ Class6-Print-score/src/hello/sortScore.java
@@ -1,5 +1,24 @@
 package hello;
-
+import java.util.Arrays;
 public class sortScore {
-
+	public static void main(String[] args) {
+		int[] scores = {89, -23, 64, 91, 119, 52, 73};
+		sortScore fcc = new sortScore();
+		System.out.println("考试成绩的前三名为：");
+		fcc.sort(scores);
+	}
+	
+	public void sort(int[] scores){
+		Arrays.sort(scores);
+		int j = 1;
+		for(int i = (scores.length - 1); i >= 0; i--){
+			if((scores[i] >= 0) && (scores[i] <= 100)){
+				j++;
+				System.out.println(scores[i]);
+			}
+			if(j > 3){
+				break;
+			}
+		}
+	}
 }
