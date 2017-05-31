aa1bc9bb6f118a6f13b5d50175fedb495b8c141f
NiYa193 <1556579665@qq.com>
2017-05-31 20:03:22 +0800

    无参有返回值方法的学习

--- Class5-Idea-In-Java/src/hello/idea.java
+++ Class5-Idea-In-Java/src/hello/idea.java
@@ -1,15 +1,19 @@
 package hello;
 
 public class idea{
-	//添加名为showMyLove的方法
-	public void showMyLove(){
-		System.out.println("Doudou I love you!");
+	public static void main(String[] args){
+		//建立一个名为stu的对象
+		idea stu = new idea();
+		//调用stu的calcAvg方法，并接收返回值
+		double stuScore = stu.calcAvg();
+		System.out.println("学生的成绩平均分是："+stuScore+"分！");
 	}
 	
-	public static void main(String[] args) {
-		//添加一个fcc对象
-		idea fcc = new idea();
-		//调用fcc对象的showMyLove方法
-		fcc.showMyLove();
+	public double calcAvg(){
+		double javaScore = 92;
+		double phpScore = 97;
+		double avg = (javaScore + phpScore) /2;
+		//返回方法的运算结果
+		return avg;
 	}
 }
