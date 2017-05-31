5bcc485dd344ed1666911bbe57da4d042cc0b3da
NiYa193 <1556579665@qq.com>
2017-05-31 19:51:06 +0800

    无参无返回值方法的学习

--- Class5-Idea-In-Java/src/hello/idea.java
+++ Class5-Idea-In-Java/src/hello/idea.java
@@ -1,14 +1,15 @@
 package hello;
 
 public class idea{
-	public void print(){
-		System.out.println("Hello World!");
+	//添加名为showMyLove的方法
+	public void showMyLove(){
+		System.out.println("Doudou I love you!");
 	}
 	
-	public static void main(String[] args){
-		idea test = new idea();
-		test.print();
+	public static void main(String[] args) {
+		//添加一个fcc对象
+		idea fcc = new idea();
+		//调用fcc对象的showMyLove方法
+		fcc.showMyLove();
 	}
-	
-	
 }
