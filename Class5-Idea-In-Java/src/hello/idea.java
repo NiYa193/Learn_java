871d44d64c136c1c7d0b6cc7f56f548c75fb9e07
NiYa193 <1556579665@qq.com>
2017-05-31 21:02:41 +0800

    方法的编程练习

--- Class5-Idea-In-Java/src/hello/idea.java
+++ Class5-Idea-In-Java/src/hello/idea.java
@@ -1,24 +1,26 @@
 package hello;
+import java.util.Arrays;
 
 public class idea{
-    public static void main(String[] args) {
-		// 创建对象
-		idea hello = new idea();
-		// 调用无参的方法
-		hello.print();
-		// 调用带有一个字符串参数的方法
-	    hello.print("Doudou I love you");
-		// 调用带有一个整型参数的方法
-	    hello.print(23);
+	public static void main(String[] args) {
+		//定义fcc对象
+		idea fcc = new idea();
+		//定义value数组，接收返回值
+		int[] value = fcc.getRandomSort(8);
+		//打印toString后的字符串
+		System.out.println(Arrays.toString(value));
 	}
-
-	public void print() {
-		System.out.println("无参的print方法");
-	}
-	public void print(String name) {
-		System.out.println("带有一个字符串参数的print方法，参数值为：" + name);
-	}
-	public void print(int age) {
-		System.out.println("带有一个整型参数的print方法，参数值为：" + age);
+	
+	public int[] getRandomSort(int arrayLength){
+		//定义一个arrayLength长度的数组
+		int[] randomValue = new int[arrayLength];
+		//循环对数组进行随机赋值
+		for(int i = 0; i < arrayLength; i++){
+			randomValue[i] =(int)(Math.random()*100);
+		}
+		//对数组进行排序
+		Arrays.sort(randomValue);
+		//返回排序好的数组
+		return randomValue;
 	}
 }
