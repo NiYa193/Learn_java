76a6137c808152872110d03343da4cfc7b6780fe
NiYa193 <1556579665@qq.com>
2017-05-31 19:41:09 +0800

    Java中的方法学习基础知识

---
+++ Class5-Idea-In-Java/src/hello/idea.java
@@ -0,0 +1,14 @@
+package hello;
+
+public class idea{
+	public void print(){
+		System.out.println("Hello World!");
+	}
+	
+	public static void main(String[] args){
+		idea test = new idea();
+		test.print();
+	}
+	
+	
+}
