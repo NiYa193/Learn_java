a50b723b68d728e677d19f9400c4f87d4e466a6f
NiYa193 <1556579665@qq.com>
2017-05-31 20:15:44 +0800

    编程练习

--- Class5-Idea-In-Java/src/hello/idea.java
+++ Class5-Idea-In-Java/src/hello/idea.java
@@ -1,19 +1,29 @@
 package hello;
 
 public class idea{
-	public static void main(String[] args){
-		//建立一个名为stu的对象
-		idea stu = new idea();
-		//调用stu的calcAvg方法，并接收返回值
-		double stuScore = stu.calcAvg();
-		System.out.println("学生的成绩平均分是："+stuScore+"分！");
+	//定义一个查找最大年龄的方法
+	public int findMaxAge(){
+		//定义年龄数组
+		int[] ages = {18, 23, 21, 19, 25, 29 ,17};
+		//假定最大年龄为数组第一个元素
+		int max = ages[0];
+		//遍历查找元素，并与max相比，查找出最大找元素
+		for(int i = 0; i < ages.length; i++){
+			if(ages[i] > max){
+				max = ages[i];
+			}
+		}
+		//返回最大年龄
+		return max;
 	}
 	
-	public double calcAvg(){
-		double javaScore = 92;
-		double phpScore = 97;
-		double avg = (javaScore + phpScore) /2;
-		//返回方法的运算结果
-		return avg;
+	public static void main(String[] args) {
+		int maxScore;
+		//新建一个fcc对象
+		idea fcc = new idea();
+		//调用fcc对象的findMaxAge方法
+		maxScore = fcc.findMaxAge();
+		//输出年龄
+		System.out.println("最大年龄为："+maxScore+"岁");
 	}
 }
