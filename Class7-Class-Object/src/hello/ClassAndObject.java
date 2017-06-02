684fbcf7a34a329ede3452871f495594bae11abe
NiYa193 <1556579665@qq.com>
2017-06-02 14:55:43 +0800

    定义Java中的类学习

--- Class7-Class-Object/src/hello/ClassAndObject.java
+++ Class7-Class-Object/src/hello/ClassAndObject.java
@@ -1,5 +1,22 @@
 package hello;
-
+//利用手机讲解类和对象的关系
+//1、定义一个类
 public class ClassAndObject {
-
+	//2、属性（成员变量）：有什么
+	float screen;
+	float cpu;
+	float memory;
+	//3、方法：干什么
+	void call(){
+		System.out.println("Telephone有打电话的功能！");
+	}
+	void sentMessage(){
+		System.out.println("Telephone有发短信的功能！");
+	}
+	public static void main(String[] args) {
+		//main函数中调用这两个方法
+		ClassAndObject fcc = new ClassAndObject();
+		fcc.call();
+		fcc.sentMessage();
+	}
 }
