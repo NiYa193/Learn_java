9d9f9f21c7ca1d63adeb24f94f0000f38c7a2e38
NiYa193 <1556579665@qq.com>
2017-06-02 20:15:29 +0800

    无参的构造方法学习
    1、使用new+构造方法创建一个新的对象
    2、构造方法是定义在Java类中的一个用来初始化对象的方法，构造方法与类同名且没有返回值

--- Class7-Class-Object/src/hello/InitialTelephone.java
+++ Class7-Class-Object/src/hello/InitialTelephone.java
@@ -1,14 +1,7 @@
 package hello;
 
-public class InitialTelephone {
-
+public class InitialTelephone{
 	public static void main(String[] args) {
-		//该类专用于新建对象
-		ClassAndObject phone = new ClassAndObject();
-		//给实例变量赋值
-		phone.screen = 5.0f;
-		phone.cpu = 1.4f;
-		phone.memory = 2.0f;
-		phone.call();
+		Telephone phone = new Telephone();
 	}
 }
---
+++ Class7-Class-Object/src/hello/Telephone.java
@@ -0,0 +1,7 @@
+package hello;
+
+public class Telephone{
+	public Telephone(){
+		System.out.println("无参的构造方法执行了！");
+	}
+}
