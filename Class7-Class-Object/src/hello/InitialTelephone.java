2fed0ab494552e9b458d9655a3fca442d27bfe19
NiYa193 <1556579665@qq.com>
2017-06-02 20:25:16 +0800

    有参的构造方法学习
    带参构造方法的使用
    main():
    Telephone phone2 = new Telephone(4.0f, 2.0f, 2.0f);
    class Telephone:
    public Telephone(float new Screen, float newCpu, float new newMemery){
    screen = newScreen;
    cpu = newCpu;
    memory = newMemory;
    }

--- Class7-Class-Object/src/hello/InitialTelephone.java
+++ Class7-Class-Object/src/hello/InitialTelephone.java
@@ -3,5 +3,8 @@ package hello;
 public class InitialTelephone{
 	public static void main(String[] args) {
 		Telephone phone = new Telephone();
+		//还需在main里面再创建一个对象
+		//且此对象要带上要传的参数值
+		Telephone newPhone = new Telephone(3.0f, 1.3f, 2.0f);
 	}
 }
--- Class7-Class-Object/src/hello/Telephone.java
+++ Class7-Class-Object/src/hello/Telephone.java
@@ -1,7 +1,22 @@
 package hello;
 
 public class Telephone{
+	float screen;
+	float cpu;
+	float memory;
 	public Telephone(){
 		System.out.println("无参的构造方法执行了！");
+	}
+	//可直接再次定义，类似方法的重载
+	public Telephone(float newScreen, float newCpu, float newMemory){
+		if(newScreen < 3.5f){
+			screen = 3.5f;
+			System.out.println("您所输入的屏幕尺寸有错误，已改为默认值。");
+		}else{
+			screen = newScreen;
+		}
+		cpu = newCpu;
+		memory = newMemory;
+		System.out.println("Screen:"+screen+" Cpu:"+cpu+" Memory:"+memory);
 	}
 }
