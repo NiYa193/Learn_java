66f34b61ca6d5d951a7c42b83e53e485a38aae52
NiYa193 <1556579665@qq.com>
2017-06-04 07:52:32 +0800

    Java中的static使用之静态方法
    静态方法的使用方法
    1、使用static关键字声明静态方法
    2、可直接使用类名调用静态方法

--- Class8-Static/src/hello/learnStatic.java
+++ Class8-Static/src/hello/learnStatic.java
@@ -1,17 +1,15 @@
 package hello;
 
-public class learnStatic {
-	//在此处声明一个static字符型变量
-	static String className = "Java开发一班";
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//在此处可直接使用类名调用这个变量
-		System.out.println("通过类名访问："+learnStatic.className);
-		//依然可以使用新建对象再使用
-		learnStatic fcc = new learnStatic();
-		System.out.println("通过对象访问："+fcc.className);
-		fcc.className = "Java开发二班";
-		System.out.println("再次通过类名访问修改后的变量："+learnStatic.className);
+public class learnStatic{
+	//在此处声明一个静态的方法
+	public static void print(){
+		System.out.println("声明的静态方法");
 	}
-
+	public static void main(String[] args) {
+		//可以使用类名调用静态方法
+		learnStatic.print();
+		//也可以使用对象名调用，更推荐使用类名调用的方式
+		learnStatic fcc = new learnStatic();
+		fcc.print();
+	}
 }
