f1f264f08596c7d285f4e180f158d676a89abd01
NiYa193 <1556579665@qq.com>
2017-06-04 07:56:09 +0800

    Java中的static使用之静态方法
    静态方法中可以直接调用同类中的静态成员，但不能直接调用非静态成员。

--- Class8-Static/src/hello/learnStatic.java
+++ Class8-Static/src/hello/learnStatic.java
@@ -1,9 +1,14 @@
 package hello;
 
 public class learnStatic{
+	String name = "fcc";
+	static String hobby = "Java";
 	//在此处声明一个静态的方法
 	public static void print(){
-		System.out.println("声明的静态方法");
+		//静态方法中不可直接调用非静态变量
+		//System.out.println("欢迎您："+name+"!");
+		//静态方法中可以直接调用静态变量
+		System.out.println("爱好："+hobby+"!");
 	}
 	public static void main(String[] args) {
 		//可以使用类名调用静态方法
