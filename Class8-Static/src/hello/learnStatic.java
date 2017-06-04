ab000a7c7ca715eedef4626f9c459d96739b3d9b
NiYa193 <1556579665@qq.com>
2017-06-04 08:08:44 +0800

    静态变量习题
    1、静态方法中可以直接访问静态变量
    2、静态方法中不可以直接访问非静态变量，要通过对象访问

--- Class8-Static/src/hello/learnStatic.java
+++ Class8-Static/src/hello/learnStatic.java
@@ -1,29 +1,19 @@
 package hello;
 
 public class learnStatic{
-	String name = "fcc";
-	static String hobby = "Java";
-	//在此处声明一个静态的方法
-	public static void print(){
-		//静态方法中不可直接调用非静态变量
-		//System.out.println("欢迎您："+name+"!");
-		//静态方法中可以直接调用静态变量
-		System.out.println("爱好："+hobby+"!");
-	}
+	//建立一个非静态变量，一个静态变量
+	int score1 = 97;
+	static int score2 = 93;
 	
-	public void normalPrint(){
-		//普通成员方法，可以直接访问非静态变量和静态变量
-		System.out.println("欢迎您"+name+"!");
-		System.out.println("爱好"+hobby+"!");
+	public static int sum(){
+		//非静态变量只能通过对象才能访问，静态变量可以直接访问
+		learnStatic fcc = new learnStatic();
+		int sumScore = fcc.score1 + score2;
+		return sumScore;
 	}
 	
 	public static void main(String[] args) {
-		//可以使用类名调用静态方法
-		learnStatic.print();
-		//也可以使用对象名调用，更推荐使用类名调用的方式
-		learnStatic fcc = new learnStatic();
-		//静态方法中，静态方法可以直接调用，但需要对象来访问非静态方法
-		print();
-		fcc.normalPrint();
+		int allScore = sum();
+		System.out.println("总成绩为："+allScore);
 	}
 }
