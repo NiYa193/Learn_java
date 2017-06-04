0a7698915b36e53e4654647f7037cee87b1e2990
NiYa193 <1556579665@qq.com>
2017-06-04 08:00:27 +0800

    Java中的static使用之静态方法
    1、如果希望在静态方法中调用非静态变量，可以通过创建类的对象，然后通过对象来访问非静态变量。
    2、在普通成员方法中，则可以直接访问同类的非静态变量和静态变量
    3、静态方法中不能直接调用非静态方法，需要通过对象来访问非静态方法。

--- Class8-Static/src/hello/learnStatic.java
+++ Class8-Static/src/hello/learnStatic.java
@@ -10,11 +10,20 @@ public class learnStatic{
 		//静态方法中可以直接调用静态变量
 		System.out.println("爱好："+hobby+"!");
 	}
+	
+	public void normalPrint(){
+		//普通成员方法，可以直接访问非静态变量和静态变量
+		System.out.println("欢迎您"+name+"!");
+		System.out.println("爱好"+hobby+"!");
+	}
+	
 	public static void main(String[] args) {
 		//可以使用类名调用静态方法
 		learnStatic.print();
 		//也可以使用对象名调用，更推荐使用类名调用的方式
 		learnStatic fcc = new learnStatic();
-		fcc.print();
+		//静态方法中，静态方法可以直接调用，但需要对象来访问非静态方法
+		print();
+		fcc.normalPrint();
 	}
 }
