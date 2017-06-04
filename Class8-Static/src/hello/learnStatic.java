c7c4ba772dc2a2a5803f6e9520719cbcd27497a9
NiYa193 <1556579665@qq.com>
2017-06-04 09:35:43 +0800

    Java中的static使用之静态初始化块
    1、在类的声明中，可以包含多个初始化块，当创建类的实例时，就会依次执行这些代码块。如果使用 static
    修饰初始化块，就称为静态初始化块。
    2、静态初始化块只在类加载时执行，且只会执行一次，
    3、静态初始化块只能给静态变量赋值，不能初始化普通的成员变量。

--- Class8-Static/src/hello/learnStatic.java
+++ Class8-Static/src/hello/learnStatic.java
@@ -1,19 +1,33 @@
 package hello;
 
 public class learnStatic{
-	//建立一个非静态变量，一个静态变量
-	int score1 = 97;
-	static int score2 = 93;
-	
-	public static int sum(){
-		//非静态变量只能通过对象才能访问，静态变量可以直接访问
-		learnStatic fcc = new learnStatic();
-		int sumScore = fcc.score1 + score2;
-		return sumScore;
+	String name;   //声明名字字符串变量
+	String sex;    //声明性别字符串变量
+	static int age;//声明年龄整形静态变量
+	//通过构造方法初始化name
+	public learnStatic(){
+		name = "fcc";
+		System.out.println("通过构造方法初始化name");
+	}
+	//通过静态初始化块初始化age
+	static{
+		age = 23;
+		System.out.println("通过静态初始化块初始化age");
+	}
+	//通过初始化块初始化sex
+	{
+		sex = "男";
+		System.out.println("通过初始化块初始化sex");
 	}
 	
+	//新建方法
+	public void show(){
+		System.out.println("姓名："+name+" 性别："+sex+" 年龄："+age);
+	}
 	public static void main(String[] args) {
-		int allScore = sum();
-		System.out.println("总成绩为："+allScore);
+		//构造方法
+		learnStatic fcc = new learnStatic();
+		//通过对象调用show方法
+		fcc.show();
 	}
 }
