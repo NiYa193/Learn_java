d1d3e0f9df3c6167b0ed8ed6510893f5de1a7a70
NiYa193 <1556579665@qq.com>
2017-06-07 20:05:44 +0800

    Java中的成员内部类
    1、 Inner 类定义在 Outer 类的内部，相当于 Outer 类的一个成员变量的位置，Inner 类可以使用任意访问控制符，如
    public 、 protected 、 private 等
    2、 Inner 类中定义的 test() 方法可以直接访问 Outer 类中的数据，而不受访问控制符的影响，如直接访问 Outer
    类中的私有属性a
    3、 定义了成员内部类后，必须使用外部类对象来创建内部类对象，而不能直接去 new 一个内部类对象，即：内部类 对象名 = 外部类对象.new
    内部类( );
    4、外部类是不能直接使用内部类的成员和方法
    5、如果外部类和内部类具有相同的成员变量或方法，内部类默认访问自己的成员变量或方法，如果要访问外部类的成员变量，可以使用 this 关键字

--- Class11-Inner-Class/src/imooc/inner/testInnerClass.java
+++ Class11-Inner-Class/src/imooc/inner/testInnerClass.java
@@ -1,15 +1,24 @@
 package imooc.inner;
 
-public class testInnerClass {
+public class testInnerClass{
+	//定义外部私有属性name
+	private String name = "外部name!";
+	//定义外部成员属性age
+	int age = 23;
+	//定义内部类
 	public class Inner{
+		String name = "内部name";
 		public void show(){
-			System.out.println("Fcc loves dou");
+			System.out.println("外部类的name调用："+ testInnerClass.this.name);
+			System.out.println("内部类的name调用："+ name);
+			System.out.println("外部age的调用："+ age);
 		}
 	}
-	
 	public static void main(String[] args) {
-		testInnerClass dou = new testInnerClass();
-		Inner fcc = dou.new Inner();	
-		fcc.show();
+		//创建外部类对象
+		testInnerClass fcc = new testInnerClass();
+		//借用外部类对象创建内部类对象
+		Inner dou = fcc.new Inner();
+		dou.show();
 	}
 }
