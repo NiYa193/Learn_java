16515219b9d7c67e6a4db6d28cb0616ada677ad6
NiYa193 <1556579665@qq.com>
2017-06-07 22:38:44 +0800

    方法内部类
    方法内部类就是内部类定义在外部类的方法中，方法内部类只在该方法的内部可见，即只在该方法内可以使用。
    注意：由于方法内部类不能在外部类的方法以外的地方使用，因此方法内部类不能使用访问控制符和
    static 修饰符。

--- Class11-Inner-Class/src/imooc/inner/testInnerClass.java
+++ Class11-Inner-Class/src/imooc/inner/testInnerClass.java
@@ -1,26 +1,26 @@
 package imooc.inner;
 
 public class testInnerClass{
-	//定义外部类静态私有变量
-	private static int score = 98;
-	//定义外部类变量
-	int outScore = 93;
-	//定义静态内部类
-	public static class inner{
-		int score = 89;
-		public void show(){
-			//访问外部类静态成员采用外部类名.静态成员
-			System.out.println("访问外部类的score："+ testInnerClass.score);
-			//访问内部成员直接访问
-			System.out.println("访问内部类的score："+ score);
-			//访问外部非静态成员 new 外部类().成员  的方式访问 
-			System.out.println("访问外部内的非私有outScore："+ new testInnerClass().outScore);
+	String name = "熊豆豆";
+	int score = 80;
+	//定义外部类中的show方法
+	public void show(){
+		//定义方法内部类
+		class inner{
+			
+			public int add(int x){
+				return x + 10;
+			}
 		}
-	}
-	
-	public static void main(String[] args) {
-		//创建静态内部类的对象时，不需外部类对象，直接创建即可
+		//新建方法内部类对象
 		inner fcc = new inner();
-		fcc.show();
+		int finalScore = fcc.add(score);
+		System.out.println("姓名："+name);
+		System.out.println("成绩："+ finalScore);
+	}
+	public static void main(String[] args) {
+		//创建外部类对象
+		testInnerClass dou = new testInnerClass();
+		dou.show();
 	}
 }
