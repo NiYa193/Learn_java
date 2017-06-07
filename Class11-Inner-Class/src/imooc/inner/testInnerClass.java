7898932a8f15f72570ed2e80687a5d1cf6650295
NiYa193 <1556579665@qq.com>
2017-06-07 22:18:19 +0800

    静态内部类
    静态内部类是 static 修饰的内部类，这种内部类的特点是：
    1、 静态内部类不能直接访问外部类的非静态成员，但可以通过 new 外部类().成员 的方式访问
    2、
    如果外部类的静态成员与内部类的成员名称相同，可通过“类名.静态成员”访问外部类的静态成员；如果外部类的静态成员与内部类的成员名称不相同，则可通过“成员名”直接调用外部类的静态成员
    3、 创建静态内部类的对象时，不需要外部类的对象，可以直接创建 内部类 对象名= new 内部类();

--- Class11-Inner-Class/src/imooc/inner/testInnerClass.java
+++ Class11-Inner-Class/src/imooc/inner/testInnerClass.java
@@ -1,24 +1,26 @@
 package imooc.inner;
 
 public class testInnerClass{
-	//定义外部私有属性name
-	private String name = "外部name!";
-	//定义外部成员属性age
-	int age = 23;
-	//定义内部类
-	public class Inner{
-		String name = "内部name";
+	//定义外部类静态私有变量
+	private static int score = 98;
+	//定义外部类变量
+	int outScore = 93;
+	//定义静态内部类
+	public static class inner{
+		int score = 89;
 		public void show(){
-			System.out.println("外部类的name调用："+ testInnerClass.this.name);
-			System.out.println("内部类的name调用："+ name);
-			System.out.println("外部age的调用："+ age);
+			//访问外部类静态成员采用外部类名.静态成员
+			System.out.println("访问外部类的score："+ testInnerClass.score);
+			//访问内部成员直接访问
+			System.out.println("访问内部类的score："+ score);
+			//访问外部非静态成员 new 外部类().成员  的方式访问 
+			System.out.println("访问外部内的非私有outScore："+ new testInnerClass().outScore);
 		}
 	}
+	
 	public static void main(String[] args) {
-		//创建外部类对象
-		testInnerClass fcc = new testInnerClass();
-		//借用外部类对象创建内部类对象
-		Inner dou = fcc.new Inner();
-		dou.show();
+		//创建静态内部类的对象时，不需外部类对象，直接创建即可
+		inner fcc = new inner();
+		fcc.show();
 	}
 }
