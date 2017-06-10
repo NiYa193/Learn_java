46e984ebe68c68ce228a53a8d5b32ada73315e7f
NiYa193 <1556579665@qq.com>
2017-06-10 09:20:22 +0800

    第三章 Java中的继承完结

--- Class12-Extends/src/fcc/learnextends/Animal.java
+++ Class12-Extends/src/fcc/learnextends/Animal.java
@@ -2,5 +2,4 @@ package fcc.learnextends;
 //默认下，继承于Object类，等同于：
 //public class Animal extends Object
 public class Animal{
-	
 }
--- Class12-Extends/src/fcc/learnextends/Initial.java
+++ Class12-Extends/src/fcc/learnextends/Initial.java
@@ -2,9 +2,21 @@ package fcc.learnextends;
 
 public class Initial{
 	public static void main(String[] args) {
-		Dog dog = new Dog();
-		//如不对toString重写，则默认打印对象的地址：
-		System.out.println(dog);
+		Dog dog001 = new Dog();
+		Dog dog002 = new Dog();
+		dog001.height = 1.1f;
+		dog001.name = "xupao";
+		dog001.weight = 76f;
+		dog002.height = 1.1f;
+		dog002.name = "xupao";
+		dog002.weight = 76f;
+		//如不对方法进行重写，则只判断方法的地址是否相同。那显然两个对象是不同的
+		//重写后，则判断重写的方法内的属性是否相同
+		if(dog001.equals(dog002)){
+			System.out.println("两个对象是相同的！");
+		}
+		else{
+			System.out.println("两个对象是不同的！");
+		}
 	}
-
 }
